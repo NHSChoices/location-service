314d866a6603f74fe37536f7b35215db93f4bc23
ANaylor <adamnaylor@nhs.net>
2015-07-23 15:15:42 +0100

    Added 1 char checks

---
+++ src/GoatTrip.RestApi/Controllers/LocationController.cs
@@ -0,0 +1,28 @@
+﻿
+namespace GoatTrip.RestApi.Controllers {
+    using System.Web.Http;
+
+    public class LocationController
+        : ApiController {
+
+        public IHttpActionResult Get(string query) {
+
+            if (IsBadRequest(query)) {
+                return new BadRequestResult(Request);
+            }
+
+            return Ok(new object());
+        }
+
+        private bool IsBadRequest(string query) {
+
+            if (string.IsNullOrEmpty(query))
+                return true;
+
+            if (query.Length <= 0)
+                return true;
+
+            return false;
+        }
+    }
+}
