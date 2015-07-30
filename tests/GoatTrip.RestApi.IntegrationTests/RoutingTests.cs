e8d5c19f4d8576e7a558b8f7d9fc4c1656359dde
ANaylor <adamnaylor@nhs.net>
2015-07-30 16:50:14 +0100

    Added house number and integration tests

---
+++ tests/GoatTrip.RestApi.IntegrationTests/RoutingTests.cs
@@ -0,0 +1,25 @@
+﻿
+namespace GoatTrip.RestApi.IntegrationTests {
+    using System.Net.Http;
+    using System.Web.Http;
+    using Controllers;
+    using Xunit;
+
+    public class RoutingTests {
+        [Fact]
+        [Trait("Category", "integration")]
+        public void AllRoutes_Always_RouteToCorrectControllerMethods() {
+            // arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://domain/location/");
+            var config = new HttpConfiguration();
+
+            // act
+            WebApiConfig.Register(config);
+            var route = WebApi.RouteRequest(config, request);
+
+            // asserts
+            Assert.Equal(typeof(LocationController), route.Controller);
+            Assert.Equal("Get", route.Action);
+        }
+    }
+}
