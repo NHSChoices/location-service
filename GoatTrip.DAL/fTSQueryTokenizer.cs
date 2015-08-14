edc5b9430378db1442a5cb15875d08f863921b23
jamestiffen <james.tiffen@hscic.gov.uk>
2015-08-14 13:06:24 +0100

    implemented tokeniser and query generator for optimed query

---
+++ GoatTrip.DAL/fTSQueryTokenizer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace GoatTrip.DAL
+{
+    public class fTSQueryTokenizer : IfTSQueryTokenizer
+    {
+        public fTSQueryTokenizer(string locationQuery)
+        {
+            Tokens = locationQuery.Split();
+        }
+
+        public string[] Tokens { get; private set; }
+
+        public string LastToken
+        {
+            get
+            {
+                if(Tokens.Length > 0)
+                    return Tokens.Last();
+                return "";
+            }
+        }
+
+        public string GetMatchQuery()
+        {
+            if (Tokens.Length > 1) return String.Join(" ",Tokens.Take(Tokens.Length -1));
+            if (Tokens.Length == 1) return Tokens.First() + "*";
+            return "";
+        }
+
+        public string GetLikeQuery()
+        {
+            if (Tokens.Length > 1) return "%" + Tokens.Last() + "%";
+            throw new InvalidOperationException("Like query not requres for single or zero tokens");
+        }
+
+    }
+}
---
+++ tests/GoatTrip.DAL.UnitTests/fTSQueryTokenizerTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoatTrip.DAL;
+using Xunit;
+namespace GoatTrip.DAL.Tests
+{
+    public class fTSQueryTokenizerTests
+    {
+
+        [Fact()]
+        public void fTSQueryTokenizer_Correct_Tokens_Test()
+        {
+            var searchQuery = "Multiple tokens Query";
+            var tokenizer = new fTSQueryTokenizer(searchQuery);
+
+            Assert.Equal(3, tokenizer.Tokens.Length);
+            Assert.Equal("Multiple", tokenizer.Tokens[0]);
+            Assert.Equal("tokens", tokenizer.Tokens[1]);
+            Assert.Equal("Query", tokenizer.Tokens[2]);
+        }
+
+        [Fact()]
+        public void GetMatchQuery_Single_Token_Test()
+        {
+            var searchQuery = "SingleToken";
+            var tokenizer = new fTSQueryTokenizer(searchQuery);
+
+            Assert.Equal("SingleToken*", tokenizer.GetMatchQuery());
+        }
+
+        [Fact()]
+        public void GetMatchQuery_Multiple_Tokens_Test()
+        {
+            var searchQuery = "Multiple Tokens Test";
+            var tokenizer = new fTSQueryTokenizer(searchQuery);
+
+            Assert.Equal("Multiple Tokens", tokenizer.GetMatchQuery());
+        }
+
+        [Fact()]
+        public void GetLikeQuery_Multiple_Tokens_Test()
+        {
+            var searchQuery = "Multiple Tokens Test";
+            var tokenizer = new fTSQueryTokenizer(searchQuery);
+
+            Assert.Equal("%Test%", tokenizer.GetLikeQuery());
+        }
+
+        [Fact()]
+        public void GetLikeQuery_Single_Token_Thows_Exeption_Test()
+        {
+            var searchQuery = "SingleToken";
+            var tokenizer = new fTSQueryTokenizer(searchQuery);
+
+            Assert.Throws<InvalidOperationException>(() => tokenizer.GetLikeQuery());
+        }
+    }
+}
