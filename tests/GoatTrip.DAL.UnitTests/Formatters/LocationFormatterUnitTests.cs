aeca75d54cfcc01e320bf52be554d949e5ab1fff
Tom Axworthy <tom.axworthy@nhs.net>
2015-09-11 11:29:24 +0100

    :hatching_chick: unit tests for formatters in DAL

---
+++ tests/GoatTrip.DAL.UnitTests/Formatters/LocationFormatterUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using GoatTrip.Common.Formatters;
+using GoatTrip.DAL.DTOs;
+using GoatTrip.DAL.Formatters;
+using Xunit;
+
+namespace GoatTrip.DAL.UnitTests.Formatters
+{
+    public class LocationFormatterUnitTests
+    {
+        public LocationFormatterUnitTests()
+        {
+            
+        }
+
+        [Fact()]
+        public void LocationDataFieldFormatConditions()
+        {
+            IFormatConditions<LocationDataField> sut = new LocationDataFieldFormatConditions();
+            Assert.False(sut.ShouldFormat(LocationDataField.PostCode));
+            Assert.True(sut.ShouldFormat(LocationDataField.Street));
+        }
+
+        [Fact()]
+        public void LocationFormatCondition()
+        {
+            IFormatConditions<string> sut = new LocationFormatConditions();
+            Assert.True(sut.ShouldFormat(LocationFields.StreetDescription));
+            Assert.False(sut.ShouldFormat(LocationFields.Postcode));
+        }
+    }
+}
